58b13a397f760fb2169e1122f2fca61af6fa90b6
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-07-05 22:19:36 +0200

    spoto la view home e welcome rispettivamente nelle cartelle admin e guest. corrego le route dove vengono caricate

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Ciao utente loggato
                 </div>
             </div>
         </div>
